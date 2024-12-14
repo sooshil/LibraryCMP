eaf0d428c901a111e25255d5f8d0e2deec983074
sooshil <sushilkaf@gmail.com>
2024-12-14 07:25:42 -0500

    details screen

--- composeApp/src/commonMain/kotlin/com/sukajee/library/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sukajee/library/book/presentation/book_detail/BookDetailScreen.kt
@@ -1,14 +1,47 @@
+@file:OptIn(ExperimentalLayoutApi::class)
+
 package com.sukajee.library.book.presentation.book_detail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.sukajee.library.book.presentation.book_detail.components.BlurredImageBackground
+import com.sukajee.library.book.presentation.book_detail.components.BookChip
+import com.sukajee.library.book.presentation.book_detail.components.ChipSize
+import com.sukajee.library.book.presentation.book_detail.components.TitledContent
+import com.sukajee.library.core.presentation.SandYellow
+import library.composeapp.generated.resources.Res
+import library.composeapp.generated.resources.description_unavailable
+import library.composeapp.generated.resources.languages
+import library.composeapp.generated.resources.pages
+import library.composeapp.generated.resources.rating
+import library.composeapp.generated.resources.synopsis
+import org.jetbrains.compose.resources.stringResource
+import kotlin.math.round
 
 @Composable
 fun BookDetailScreenRoot(
@@ -20,10 +53,11 @@ fun BookDetailScreenRoot(
     BookDetailScreen(
         state = state.value,
         onAction = { action ->
-            when(action) {
+            when (action) {
                 is BookDetailAction.OnBackClick -> {
                     navController.navigateUp()
                 }
+
                 else -> Unit
             }
             viewModel::onAction
@@ -50,9 +84,122 @@ fun BookDetailScreen(
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
         ) {
-            Text(text = "Book Detail")
+            if (state.book != null) {
+                Column(
+                    modifier = Modifier
+                        .widthIn(max = 700.dp)
+                        .fillMaxWidth()
+                        .padding(
+                            vertical = 16.dp,
+                            horizontal = 24.dp
+                        )
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = state.book.title ?: "",
+                        style = MaterialTheme.typography.headlineSmall,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = state.book.authors?.joinToString() ?: "",
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center
+                    )
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        state.book.averageRating?.let { rating ->
+                            TitledContent(
+                                title = stringResource(Res.string.rating),
+                            ) {
+                                BookChip {
+                                    Text(
+                                        text = "${round(rating * 10) / 10.0}"
+                                    )
+                                    Icon(
+                                        imageVector = Icons.Default.Star,
+                                        contentDescription = null,
+                                        tint = SandYellow
+                                    )
+                                }
+                            }
+                        }
+                        state.book.numPages?.let { pageCount ->
+                            TitledContent(
+                                title = stringResource(Res.string.pages),
+                            ) {
+                                BookChip {
+                                    Text(text = pageCount.toString())
+                                }
+                            }
+                        }
+                    }
+                    if (state.book.languages?.isNotEmpty() == true) {
+                        TitledContent(
+                            title = stringResource(Res.string.languages),
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        ) {
+                            FlowRow(
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier.wrapContentSize(Alignment.Center)
+                            ) {
+                                state.book.languages.forEach { language ->
+                                    BookChip(
+                                        size = ChipSize.Small,
+                                        modifier = Modifier.padding(2.dp)
+                                    ) {
+                                        Text(
+                                            text = language.uppercase(),
+                                            style = MaterialTheme.typography.bodyMedium
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Text(
+                        text = stringResource(Res.string.synopsis),
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .fillMaxWidth()
+                            .padding(
+                                top = 24.dp,
+                                bottom = 8.dp
+                            )
+                    )
+                    if (state.isLoading) {
+                        CircularProgressIndicator()
+//                    Box(
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .weight(1f),
+//                        contentAlignment = Alignment.Center
+//                    ) {
+//                    }
+                    } else {
+                        Text(
+                            text = if (state.book.description.isNullOrBlank()) {
+                                stringResource(Res.string.description_unavailable)
+                            } else {
+                                state.book.description
+                            },
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Justify,
+                            color = if (state.book.description.isNullOrBlank()) {
+                                Color.Black.copy(alpha = 0.4f)
+                            } else Color.Black,
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        )
+                    }
+                }
+            }
         }
     }
 }
