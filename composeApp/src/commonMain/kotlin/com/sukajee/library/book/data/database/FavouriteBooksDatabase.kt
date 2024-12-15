6d9062c6aef59b2fd3f388929c386b64532cdfb4
sooshil <sushilkaf@gmail.com>
2024-12-15 10:08:01 -0500

    iOS build issue fix

--- composeApp/src/commonMain/kotlin/com/sukajee/library/book/data/database/FavouriteBooksDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/sukajee/library/book/data/database/FavouriteBooksDatabase.kt
@@ -1,11 +1,13 @@
 package com.sukajee.library.book.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [BookEntity::class], version = 1)
 @TypeConverters(StringListTypeConverter::class)
+@ConstructedBy(BookDatabaseConstructor::class)
 abstract class FavouriteBooksDatabase: RoomDatabase() {
     abstract val dao: FavouriteBooksDao
 
